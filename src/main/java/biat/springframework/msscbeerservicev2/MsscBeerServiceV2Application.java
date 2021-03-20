8bfb601b0df1dde99828de881724df03bed8bcf5
aymen braiek <aymen.braiek@esprit.tn>
2021-03-20 21:26:57 +0100

    initial spring boot project creaction  -closes #1

---
+++ src/main/java/biat/springframework/msscbeerservicev2/MsscBeerServiceV2Application.java
@@ -0,0 +1,13 @@
+package biat.springframework.msscbeerservicev2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsscBeerServiceV2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsscBeerServiceV2Application.class, args);
+	}
+
+}
