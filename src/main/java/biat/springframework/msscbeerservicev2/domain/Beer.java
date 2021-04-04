eaa58252039be70da281833a11b1d739dfb58824
aymenbraiek <aymen.braiek@esprit.tn>
2021-04-04 12:37:09 +0100

    Create JPA Entity for Beer objects  -closes #6

---
+++ src/main/java/biat/springframework/msscbeerservicev2/domain/Beer.java
@@ -0,0 +1,47 @@
+package biat.springframework.msscbeerservicev2.domain;
+
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.sql.Timestamp;
+import java.util.UUID;
+
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+public class Beer {
+
+    @Id
+    @GeneratedValue(generator = "UUID")
+    @GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator")
+    private UUID id;
+    @Version
+    private Long version;
+    //annotation for JPA
+    @CreationTimestamp
+    //this col is not enable to update
+    @Column(updatable = false)
+    private Timestamp createdDate;
+
+    @UpdateTimestamp
+    private Timestamp lastModifiedDate;
+
+    private String beerName;
+    private String beerStyle;
+
+    @Column(unique = true)
+    private String upc;
+
+    private BigDecimal price;
+
+    private Integer minOnHand;
+    private Integer quantityToBrew;
+}
--- src/test/java/biat/springframework/msscbeerservicev2/web/Controller/BeerControllerTest.java
+++ src/test/java/biat/springframework/msscbeerservicev2/web/Controller/BeerControllerTest.java
@@ -7,22 +7,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.http.MediaType;
-import org.springframework.test.web.servlet.MockMvc;
-
 import java.util.UUID;
-
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import java.util.UUID;
 
-import static org.junit.jupiter.api.Assertions.*;
 @WebMvcTest(BeerController.class)
 class BeerControllerTest {
    @Autowired
